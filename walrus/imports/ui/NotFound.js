155005deafe0e29f9abade38b69f658fc8674a3b
laskjk <deshengchuan@hotmail.com>
2017-07-12 20:45:43 +0800

    1st commit

---
+++ walrus/imports/ui/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="boxed-view">
+      <div className="boxed-view__box">
+        <h1>404 - Page Not Found</h1>
+        <p>We're unable to find that page.</p>
+      </div>
+    </div>
+  );
+};
