5be0951e3c6f8127f16ca2db8d8983c257aadfe6
ngtinghao <tinghao94@gmail.com>
2017-07-15 13:13:51 +0800

    removed appbody

--- walrus/imports/ui/App.js
+++ walrus/imports/ui/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
 import AppHeaderBar from './AppHeaderBar';
-import AppBody from './AppBody';
+import AppSideBar from './AppSideBar';
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <AppHeaderBar />
-                <AppBody />
+                <div id="wrapper" className="col-sm-2">
+                        <AppSideBar />
+                </div>
+                <div className="col-sm-offset-2 col-sm-10">
+                    <p>Your content here</p>
+                </div>
             </div>
         );
     }
