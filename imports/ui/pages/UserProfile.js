70a9d7bec837bca47ed31f54b0f7705ae531d89b
Ting Hao Ng <tinghao94@gmail.com>
2017-07-24 00:04:30 +0800

    modified userprofile ui

--- imports/ui/pages/UserProfile.js
+++ imports/ui/pages/UserProfile.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { Grid, Well } from 'react-bootstrap';
+import { Grid, Well, PageHeader } from 'react-bootstrap';
 export default class UserProfile extends Component{
 
   renderUserInfo(){
     const ratings = this.props.user.profile.ratings;
     return ratings.map((rating) => {
       return(
-        <Well key={rating.topic}>
-        <h4>{rating.topic}</h4>
-        <hr />
-        {rating.score}
-        </Well>
+        <div>
+            <Well key={rating.topic}>
+            <h4>{rating.topic.replace("_"," : ")}</h4>
+            <p>{rating.score}</p>
+            </Well>
+        </div>
       )
     })
     console.log(ratings);
@@ -20,12 +21,14 @@ export default class UserProfile extends Component{
   renderDefault(){
     if(this.props.user.profile.ratings.length === 0){
       return <Well> {this.props.username} has not posted any answers yet </Well>
-    }
+  } else {
+      return <PageHeader> {this.props.username+"  "}<small>Profile </small> </PageHeader>
+  }
   }
   render(){
     console.log(this.props)
     return(
-      <Grid>
+      <Grid className="answer-container">
         {this.renderDefault()}
         {this.renderUserInfo()}
       </Grid>
