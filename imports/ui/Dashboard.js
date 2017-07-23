7ae3b102d3f67ca673e360e85f4f6728098669d4
Ong Jia Rui <ongjiarui@hotmail.com>
2017-07-23 22:42:37 +0800

    Edited fonts

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -25,13 +25,17 @@ const styles = {
     backgroundColor: '#f7f7f7',
   },
 
+  title: {
+    fontSize: 15,
+  },
+
   text: {
-    fontSize: 13,
+    fontSize: 12,
   },
 
   link: {
     color: '#00BBE8',
-    fontSize: 13,
+    fontSize: 12,
 
   }
 }
@@ -43,22 +47,22 @@ export default class Dashboard extends Component {
       <div style={styles.page}>
         <div className = "dashboard-summary" style={styles.summary}>
           <div style={styles.summaryDiv}>
-            <h5>Trending Status</h5>
+            <h3 style={styles.title}>Trending Status</h3>
             <p style={styles.text}>Popularity: Rapidly Increasing</p>
             <p style={styles.text}>Momentum: Very Strong</p>
           </div>
           <div style={styles.summaryDiv}>
-            <h5>Enable Precision Metrics</h5>
+            <h3 style={styles.title}>Enable Precision Metrics</h3>
             <p style={styles.text}>Precision Metrics provides detailed hourly topic analysis</p>
             <a href='#' style={styles.link}><p>ENABLE PRECISION METRICS</p></a>
           </div>
           <div style={styles.summaryDiv}>
-            <h5>Privacy Controls</h5>
+            <h3 style={styles.title}>Privacy Controls</h3>
             <p style={styles.text}>Popularity: Rapidly Increasing</p>
             <a href='#' style={styles.link}><p>ENABLE PRIVACY CONTROLS</p></a>
           </div>
           <div style={styles.summaryDiv}>
-            <h5>Service Loadtest</h5>
+            <h3 style={styles.title}>Service Loadtest</h3>
             <p style={styles.text}>Test service's down link and up link speed to handle large user load.</p>
             <a href='#' style={styles.link}><p>TEST SERVICE</p></a>
           </div>
